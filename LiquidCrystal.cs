df42d373aa8e1d0140c1f2f72428af9e511ba46a
Alessandro Miranda <alecsandrius@hotmail.it>
2022-05-14 00:20:33 +0200

    refactoring

--- LiquidCrystal.cs
+++ LiquidCrystal.cs
@@ -1,37 +1,56 @@
-﻿using System.Device;
-using System.Device.Gpio;
+﻿using System.Device.Gpio;
 using System.Threading;
+using static System.Device.DelayHelper;
 
 namespace LCD1602
 {
-    public class ControlDisplay
+    public class DisplayOnOffControl
     {
-        public readonly uint LCD_DISPLAYON = 0x04;
-        public readonly uint LCD_DISPLAYOFF = 0x00;
-        public readonly uint LCD_CURSORON = 0x02;
-        public readonly uint LCD_CURSOROFF = 0x00;
-        public readonly uint LCD_BLINKON = 0x01;
-        public readonly uint LCD_BLINKOFF = 0x00;
+        private const uint DisplayOn = 0x04;
+        private readonly uint LCD_DISPLAYOFF = 0x00;
+        private readonly uint LCD_CURSORON = 0x02;
+        private const uint CursorOff = 0x00;
+        private const uint BlinkOn = 0x01;
+        private const uint BlinkOff = 0x00;
 
-        public ControlDisplay()
+        private uint _value;
+
+        public DisplayOnOffControl()
         {
+            // turn the display on with no cursor or blinking default
+            _value = DisplayOn | CursorOff | BlinkOff;
         }
+
+        public uint NoDisplay => _value &= ~DisplayOn;
+        public uint Display => _value |= DisplayOn;
+        public uint Blink => _value |= BlinkOn;
+        public uint NoBlink => _value &= ~BlinkOn;
     }
 
     public class Command
     {
+        private readonly DisplayOnOffControl _displayOnOffControl;
         public readonly uint LCD_RETURNHOME = 0x02;
+
         public readonly uint LCD_ENTRYMODESET = 0x04;
-        public readonly uint LCD_DISPLAYCONTROL = 0x08;
+        private readonly uint _displaycontrol = 0x08;
         public readonly uint LCD_CURSORSHIFT = 0x10;
         public readonly uint LCD_FUNCTIONSET = 0x20;
         public readonly uint LCD_SETCGRAMADDR = 0x40;
         public readonly uint LCD_SETDDRAMADDR = 0x80;
-        public readonly uint Cleardisplay = 0x01;
 
-        public Command()
+        public Command(DisplayOnOffControl displayOnOffControl)
         {
+            _displayOnOffControl = displayOnOffControl;
         }
+
+        public uint ClearDisplay => 0x01;
+
+        public uint NoDisplay => _displaycontrol | _displayOnOffControl.NoDisplay;
+        public uint Display => _displaycontrol | _displayOnOffControl.Display;
+        public uint NoBlink => _displaycontrol | _displayOnOffControl.NoBlink;
+        public uint Blink => _displaycontrol | _displayOnOffControl.Blink;
+
     }
 
     public class LiquidCrystal
@@ -39,10 +58,10 @@ namespace LCD1602
         // commands
 
         // flags for display entry mode     
-        readonly uint LCD_ENTRYRIGHT = 0x00;
-        readonly uint LCD_ENTRYLEFT = 0x02;
-        readonly uint LCD_ENTRYSHIFTINCREMENT = 0x01;
-        readonly uint LCD_ENTRYSHIFTDECREMENT = 0x00;
+        readonly uint _entryRight = 0x00;
+        readonly uint _entryLeft = 0x02;
+        private readonly uint _entryShiftIncrement = 0x01;
+        readonly uint _entryShiftDecrement = 0x00;
 
         // flags for display on/off control 
 
@@ -61,221 +80,167 @@ namespace LCD1602
         readonly uint LCD_5x8DOTS = 0x00;
 
 
-        int _rs_pin; // LOW: command. HIGH: character.
-        int _rw_pin; // LOW: write to LCD. HIGH: read from LCD.
-        int _enable_pin; // activated by a HIGH pulse.
-        int[] _data_pins = new int[8];
+        readonly int _rsPin; // LOW: command. HIGH: character.
+        readonly int _rwPin; // LOW: write to LCD. HIGH: read from LCD.
+        readonly int _enablePin; // activated by a HIGH pulse.
+        readonly int[] _dataPins = new int[8];
 
-        uint _displayfunction;
-        uint _displaycontrol;
-        uint _displaymode;
+        uint _displayFunction;
+        uint _displayMode;
 
         uint _initialized;
         uint _numlines;
 
-        readonly uint[] _row_offsets = new uint[4];
-        private GpioController _gpio;
-        private readonly ControlDisplay _controlDisplay;
-        private readonly Command _command;
+        readonly uint[] _rowOffsets = new uint[4];
+        private readonly GpioController _gpio = new();
+        private readonly Command _command = new(new DisplayOnOffControl());
 
         public LiquidCrystal(int rs, int enable,
-                 int d0, int d1, int d2, int d3)
+                 int d0, int d1, int d2, int d3, uint lines = 1, uint dotSize = 0x00)
         {
-            _gpio = new GpioController();
-            Init(1, rs, 255, enable, d0, d1, d2, d3, 0, 0, 0, 0);
-            _controlDisplay = new ControlDisplay();
-            _command = new Command();
-        }
-
-        private void Init(uint fourbitmode, int rs, int rw, int enable,
-             int d0, int d1, int d2, int d3,
-             int d4, int d5, int d6, int d7)
-        {
-            _rs_pin = rs;
-            _rw_pin = rw;
-            _enable_pin = enable;
-
-            _data_pins[0] = d0;
-            _data_pins[1] = d1;
-            _data_pins[2] = d2;
-            _data_pins[3] = d3;
-            _data_pins[4] = d4;
-            _data_pins[5] = d5;
-            _data_pins[6] = d6;
-            _data_pins[7] = d7;
-
-            if (fourbitmode == 1)
-                _displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-            else
-                _displayfunction = LCD_8BITMODE | LCD_1LINE | LCD_5x8DOTS;
-
-            Begin(16, 1);
-        }
-
-        public void Begin(uint cols, uint lines, uint dotsize = 0x00)
-        {
+            _rsPin = rs;
+            _rwPin = 255;
+            _enablePin = enable;
             _numlines = lines;
+            _dataPins[0] = d0;
+            _dataPins[1] = d1;
+            _dataPins[2] = d2;
+            _dataPins[3] = d3;
+            _dataPins[4] = 0;
+            _dataPins[5] = 0;
+            _dataPins[6] = 0;
+            _dataPins[7] = 0;
+
+            _displayFunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
+
+            //  _displayFunction = LCD_8BITMODE | LCD_1LINE | LCD_5x8DOTS;
+            if ((dotSize != LCD_5x8DOTS) && (lines == 1)) 
+                _displayFunction |= LCD_5x10DOTS;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            const uint cols = 16;
             SetRowOffsets(0x00, 0x40, 0x00 + cols, 0x40 + cols);
 
-            if ((dotsize != LCD_5x8DOTS) && (lines == 1))
-            {
-                _displayfunction |= LCD_5x10DOTS;
-            }
-
-            _gpio.OpenPin(_rs_pin, PinMode.Output);
-
-            if (_rw_pin != 255)
-            {
-                _gpio.OpenPin(_rw_pin, PinMode.Output);
-            }
-            _gpio.OpenPin(_enable_pin, PinMode.Output);
+            _gpio.OpenPin(_rsPin, PinMode.Output);
+            if (_rwPin != 255) 
+                _gpio.OpenPin(_rwPin, PinMode.Output);
+            _gpio.OpenPin(_enablePin, PinMode.Output);
 
             // Do these once, instead of every time a character is drawn for speed reasons.
-            for (int i = 0; i < ((_displayfunction & LCD_8BITMODE) == 1 ? 8 : 4); ++i)
-            {
-                _gpio.OpenPin(_data_pins[i], PinMode.Output);
-            }
+            for (var i = 0; i < ((_displayFunction & LCD_8BITMODE) == 1 ? 8 : 4); ++i)
+                _gpio.OpenPin(_dataPins[i], PinMode.Output);
 
             // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
             // according to datasheet, we need at least 40 ms after power rises above 2.7 V
             // before sending commands. Arduino can turn on way before 4.5 V so we'll wait 50
             Thread.Sleep(50);
             // Now we pull both RS and R/W low to begin commands
-            _gpio.Write(_rs_pin, PinValue.Low);
-            _gpio.Write(_enable_pin, PinValue.Low);
+            _gpio.Write(_rsPin, PinValue.Low);
+            _gpio.Write(_enablePin, PinValue.Low);
 
-            if (_rw_pin != 255)
-                _gpio.Write(_rw_pin, PinValue.Low);
+            if (_rwPin != 255)
+                _gpio.Write(_rwPin, PinValue.Low);
 
             //put the LCD into 4 bit or 8 bit mode
-            if ((_displayfunction & LCD_8BITMODE) == 0)
+            if ((_displayFunction & LCD_8BITMODE) == 0)
             {
                 // this is according to the Hitachi HD44780 datasheet
                 // figure 24, pg 46
                 // we start in 8bit mode, try to set 4 bit mode
-                Write4bits(0x03);
-                DelayHelper.DelayMicroseconds(4500, false); // wait min 4.1ms 
+                Write4Bits(0x03);
+                DelayMicroseconds(4500, false); // wait min 4.1ms 
 
                 // second try
-                Command(_command.LCD_FUNCTIONSET | _displayfunction);
-                DelayHelper.DelayMicroseconds(150, false);
+                Command(_command.LCD_FUNCTIONSET | _displayFunction);
+                DelayMicroseconds(150, false);
 
                 // third go
-                Command(_command.LCD_FUNCTIONSET | _displayfunction);
+                Command(_command.LCD_FUNCTIONSET | _displayFunction);
             }
             // finally, set # lines, font size, etc.
-            Command(_command.LCD_FUNCTIONSET | _displayfunction);
+            Command(_command.LCD_FUNCTIONSET | _displayFunction);
 
-            // turn the display on with no cursor or blinking default
-            _displaycontrol = _controlDisplay.LCD_DISPLAYON | _controlDisplay.LCD_CURSOROFF | _controlDisplay.LCD_BLINKOFF;
             Display();
 
             // clear it off
             Clear();
 
             // Initialize to default text direction (for romance languages)
-            _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+            _displayMode = _entryLeft | _entryShiftDecrement;
             // set the entry mode
-            Command(_command.LCD_ENTRYMODESET | _displaymode);
+            Command(_command.LCD_ENTRYMODESET | _displayMode);
         }
-        void Command(uint value)
+
+        private void Command(uint value)
         {
             Send(value, PinValue.Low);
         }
-
-
-        public void Write(uint value)
+        private void Send(uint value, PinValue mode)
         {
-            Send(value, PinValue.High);
-        }
-
-
-        void Send(uint value, PinValue mode)
-        {
-            _gpio.Write(_rs_pin, mode);
+            _gpio.Write(_rsPin, mode);
 
             // if there is a RW pin indicated, set it low to Write
-            if (_rw_pin != 255)
+            if (_rwPin != 255)
             {
-                _gpio.Write(_rw_pin, PinValue.Low);
+                _gpio.Write(_rwPin, PinValue.Low);
             }
 
-            if ((_displayfunction & LCD_8BITMODE) == 1)
+            if ((_displayFunction & LCD_8BITMODE) == 1)
             {
-                Write8bits(value);
+                Write8Bits(value);
             }
             else
             {
-                Write4bits(value >> 4);
-                Write4bits(value);
+                Write4Bits(value >> 4);
+                Write4Bits(value);
             }
         }
-        void SetRowOffsets(uint row0, uint row1, uint row2, uint row3)
+        private void SetRowOffsets(uint row0, uint row1, uint row2, uint row3)
         {
-            _row_offsets[0] = row0;
-            _row_offsets[1] = row1;
-            _row_offsets[2] = row2;
-            _row_offsets[3] = row3;
+            _rowOffsets[0] = row0;
+            _rowOffsets[1] = row1;
+            _rowOffsets[2] = row2;
+            _rowOffsets[3] = row3;
         }
-
-        void PulseEnable()
+        private void PulseEnable()
         {
-            _gpio.Write(_enable_pin, PinValue.Low);
-            DelayHelper.DelayMicroseconds(1, false);
-            _gpio.Write(_enable_pin, PinValue.High);
-            DelayHelper.DelayMicroseconds(1, false);    // enable pulse must be >450 ns
-            _gpio.Write(_enable_pin, PinValue.Low);
-            DelayHelper.DelayMicroseconds(100, false);   // commands need >37 us to settle
+            _gpio.Write(_enablePin, PinValue.Low);
+            DelayMicroseconds(1, false);
+            _gpio.Write(_enablePin, PinValue.High);
+            DelayMicroseconds(1, false);    // enable pulse must be >450 ns
+            _gpio.Write(_enablePin, PinValue.Low);
+            DelayMicroseconds(100, false);   // commands need >37 us to settle
         }
-
-        private void Write4bits(uint value)
+        private void Write4Bits(uint value)
         {
-            for (int i = 0; i < 4; i++)
-            {
-                _gpio.Write(_data_pins[i], ((int)value >> i) & 0x01);
-            }
-
+            for (var i = 0; i < 4; i++)
+                _gpio.Write(_dataPins[i], ((int)value >> i) & 0x01);
             PulseEnable();
         }
-
-        void Write8bits(uint value)
+        private void Write8Bits(uint value)
         {
-            for (int i = 0; i < 8; i++)
-            {
-                _gpio.Write(_data_pins[i], ((int)value >> i) & 0x01);
-            }
+            for (var i = 0; i < 8; i++)
+                _gpio.Write(_dataPins[i], ((int)value >> i) & 0x01);
             PulseEnable();
         }
 
         /********** high level commands, for the user! */
+        public void Write(uint value) => Send(value, PinValue.High);
+        public void Write(string message)
+        {
+            foreach (char c in message) Write(c);
+        }
         public void Clear()
         {
-            Command(_command.Cleardisplay);  // clear display, set cursor position to zero
-            DelayHelper.DelayMicroseconds(2000, false);  // this command takes a long time!
+            Send(_command.ClearDisplay, PinValue.Low);  // clear display, set cursor position to zero
+            DelayMicroseconds(2000, false);  // this command takes a long time!
         }
-
-        // Turn the display on/off (quickly)
-        void NoDisplay()
-        {
-            _displaycontrol &= ~_controlDisplay.LCD_DISPLAYON;
-            Command(_command.LCD_DISPLAYCONTROL | _displaycontrol);
-        }
-        void Display()
-        {
-            _displaycontrol |= _controlDisplay.LCD_DISPLAYON;
-            Command(_command.LCD_DISPLAYCONTROL | _displaycontrol);
-        }
-
-        // Turn on and off the blinking cursor
-        void NoBlink()
-        {
-            _displaycontrol &= ~_controlDisplay.LCD_BLINKON;
-            Command(_command.LCD_DISPLAYCONTROL | _displaycontrol);
-        }
-        void Blink()
-        {
-            _displaycontrol |= _controlDisplay.LCD_BLINKON;
-            Command(_command.LCD_DISPLAYCONTROL | _displaycontrol);
-        }
+        public void NoDisplay() => Command(_command.NoDisplay);
+        public void Display() => Command(_command.Display);
+        public void NoBlink() => Command(_command.NoBlink);
+        public void Blink() => Command(_command.Blink);
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,24 +10,18 @@ namespace TestLiquidCrystal
 
             Thread.Sleep(5000);
             const int rs = 4, en = 2, d4 = 32, d5 = 33, d6 = 25, d7 = 26;
-            var lcd = new LiquidCrystal(rs, en, d4, d5, d6, d7);
+            var lcd = new LiquidCrystal(rs, en, d4, d5, d6, d7, 1);
             // set up the LCD's number of columns and rows:
             // Print a message to the LCD.
-            string message = "hello world!";
             while (true)
             {
-                foreach(char c in message)
-                {
-                    lcd.Write(c);
-                }
+                lcd.Write("hello world!");
                 
                 Thread.Sleep(10000);
                 lcd.Clear();
-                
+
             }
             
-          
-
         }
 
 
