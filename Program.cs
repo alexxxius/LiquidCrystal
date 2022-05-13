7990370b142dec8ee2911c77279e34d4618f9f19
Alessandro Miranda <alecsandrius@hotmail.it>
2022-05-13 21:48:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using LCD;
+using System.Threading;
+
+namespace TestLiquidCrystal
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Thread.Sleep(5000);
+            const int rs = 4, en = 2, d4 = 32, d5 = 33, d6 = 25, d7 = 26;
+            var lcd = new LiquidCrystal(rs, en, d4, d5, d6, d7);
+            // set up the LCD's number of columns and rows:
+            // Print a message to the LCD.
+            string message = "hello world!";
+            while (true)
+            {
+                foreach(char c in message)
+                {
+                    lcd.Write(c);
+                }
+                
+                Thread.Sleep(10000);
+                lcd.Clear();
+                
+            }
+            
+          
+
+        }
+
+
+
+    }
+}
