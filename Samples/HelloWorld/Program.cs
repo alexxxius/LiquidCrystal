519d82e19d63228a8f3431ad76cd9fb418b164a0
Alessandro Miranda <alecsandrius@hotmail.it>
2022-05-17 14:08:33 +0200

    restructure project and folder samples

--- Samples/HelloWorld/Program.cs
+++ Samples/HelloWorld/Program.cs
@@ -1,26 +1,18 @@
-using LCD1602;
+using System.Threading;
 
-namespace LiquidCrystal
+namespace HelloWorld
 {
     public class Program
     {
         public static void Main()
         {
-
             const int rs = 4, en = 2, d4 = 32, d5 = 33, d6 = 25, d7 = 26;
-            var lcd = new LiquidCrystal(rs, en, d4, d5, d6, d7, 1);
+            var lcd = new LiquidCrystal.LiquidCrystal(rs, en, d4, d5, d6, d7, 1);
             // set up the LCD's number of columns and rows:
             // Print a message to the LCD.
             lcd.Write("hello world!");
-            while (true)
-            {
-                lcd.DisplayCursor();
-            }
+            Thread.Sleep(Timeout.Infinite);
 
         }
-
-
-
-     
     }
 }
